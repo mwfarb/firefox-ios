768a363175ccd1978504ec051fc40d3cc2825aba
Vanna Phong <vphong.dev@gmail.com>
2020-06-17 10:54:35 -0700

    merge master

---
+++ Client/Frontend/Login Management/BreachAlertsClient.swift
@@ -0,0 +1,49 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Shared
+
+/// Errors related to BreachAlertsClient and BreachAlertsManager.
+struct BreachAlertsError: MaybeErrorType {
+    public let description: String
+}
+/// For mocking and testing BreachAlertsClient.
+protocol BreachAlertsClientProtocol {
+    func fetchData(endpoint: BreachAlertsClient.Endpoint, completion: @escaping (_ result: Maybe<Data>) -> Void)
+}
+
+/// Handles all network requests for BreachAlertsManager.
+public class BreachAlertsClient: BreachAlertsClientProtocol {
+    private var dataTask: URLSessionDataTask?
+    public enum Endpoint: String {
+        case breachedAccounts = "https://monitor.firefox.com/hibp/breaches"
+    }
+
+    /// Makes a network request to an endpoint and hands off the result to a completion handler.
+    public func fetchData(endpoint: Endpoint, completion: @escaping (_ result: Maybe<Data>) -> Void) {
+        // endpoint.rawValue is the url
+        guard let url = URL(string: endpoint.rawValue) else {
+            completion(Maybe(failure: BreachAlertsError(description: "bad endpoint URL")))
+            return
+        }
+        dataTask?.cancel()
+        dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard validatedHTTPResponse(response) != nil else {
+                completion(Maybe(failure: BreachAlertsError(description: "invalid HTTP response")))
+                return
+            }
+            if let error = error {
+                completion(Maybe(failure: BreachAlertsError(description: error.localizedDescription)))
+                return
+            }
+            guard let data = data, !data.isEmpty else {
+                completion(Maybe(failure: BreachAlertsError(description: "empty data")))
+                return
+            }
+            completion(Maybe(success: data))
+        }
+        dataTask?.resume()
+    }
+}
---
+++ Client/Frontend/Login Management/BreachAlertsManager.swift
@@ -0,0 +1,91 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Storage // or whichever module has the LoginsRecord class
+import Shared // or whichever module has the Maybe class
+
+/// Breach structure decoded from JSON
+struct BreachRecord: Codable, Equatable {
+    var name: String
+    var title: String
+    var domain: String
+    var breachDate: String
+    var description: String
+
+    enum CodingKeys: String, CodingKey {
+        case name = "Name"
+        case title = "Title"
+        case domain = "Domain"
+        case breachDate = "BreachDate"
+        case description = "Description"
+    }
+}
+
+/// A manager for the user's breached login information, if any.
+final public class BreachAlertsManager {
+    var breaches: [BreachRecord] = []
+    var breachAlertsClient: BreachAlertsClientProtocol
+
+    init(_ client: BreachAlertsClientProtocol = BreachAlertsClient()) {
+        self.breachAlertsClient = client
+    }
+
+    /// Loads breaches from Monitor endpoint using BreachAlertsClient.
+    ///    - Parameters:
+    ///         - completion: a completion handler for the processed breaches
+    func loadBreaches(completion: @escaping (Maybe<[BreachRecord]>) -> Void) {
+        print("loadBreaches(): called")
+
+        self.breachAlertsClient.fetchData(endpoint: .breachedAccounts) { maybeData in
+            guard let data = maybeData.successValue else {
+                completion(Maybe(failure: BreachAlertsError(description: "failed to load breaches data")))
+                return
+            }
+            guard let decoded = try? JSONDecoder().decode([BreachRecord].self, from: data) else {
+                completion(Maybe(failure: BreachAlertsError(description: "JSON data decode failure")))
+                return
+            }
+
+            self.breaches = decoded
+            completion(Maybe(success: self.breaches))
+        }
+    }
+
+    /// Compares a list of logins to a list of breaches and returns breached logins.
+    ///    - Parameters:
+    ///         - logins: a list of logins to compare breaches to
+    func compareToBreaches(_ logins: [LoginRecord]) -> Maybe<[LoginRecord]> {
+        var result: [LoginRecord] = []
+
+        if self.breaches.count <= 0 {
+            return Maybe(failure: BreachAlertsError(description: "cannot compare to an empty list of breaches"))
+        } else if logins.count <= 0 {
+            return Maybe(failure: BreachAlertsError(description: "cannot compare to an empty list of logins"))
+        }
+
+        // TODO: optimize this loop
+        for login in logins {
+            for breach in self.breaches {
+                // host check
+                let loginHostURL = URL(string: login.hostname)
+                if loginHostURL?.baseDomain == breach.domain {
+                    print("compareToBreaches(): breach: \(breach.domain)")
+
+                    // date check
+                    let pwLastChanged = Date(timeIntervalSince1970: TimeInterval(login.timePasswordChanged))
+
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "yyyy-MM-dd"
+                    if let breachDate = dateFormatter.date(from: breach.breachDate), pwLastChanged < breachDate {
+                        print("compareToBreaches(): ⚠️ password exposed ⚠️: \(breach.breachDate)")
+                        result.append(login)
+                    }
+                }
+            }
+        }
+        print("compareToBreaches(): fin")
+        return Maybe(success: result)
+    }
+}
